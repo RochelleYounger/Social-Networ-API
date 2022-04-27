d7da15c3d48fe31db8e731d4fe505d9465979762
Rochelle Younger <regnuoy321@gmail.com>
2022-04-27 06:43:46 -0400

    finished up models

--- models/User.js
+++ models/User.js
@@ -1,18 +1,42 @@
 const { Schema, model } = require('mongoose');
 
-const UserSchema = new Schema({
-  username: {
-    type: String,
-    unique: true,
-    required: true,
-    trim: true
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: 'Field cannot be left empty',
+      trim: true
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: 'Field cannot be left empty',
+      match: [/.+\@.+\..+/, 'Email must be properly formatted.']
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
+      }
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+      }
+    ]
   },
-  email: {
-    type: String,
-    unique: true,
-    required: true,
-    match: [/.+\@.+\..+/]
+  {
+    toJSON: {
+      virtuals: true
+    },
+    id: false
   }
+);
+
+UserSchema.virtual('friendCount').get(function() {
+  return this.friends.length;
 });
 
 const User = model('User', UserSchema);
